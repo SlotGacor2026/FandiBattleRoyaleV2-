845ab20cef886034f70c89a6a81ed93b03fc13da
SlotGacor2026 <fandieko758@gmail.com>
2026-01-20 19:59:50 +0700

    Update player.js

--- js/game/player.js
+++ js/game/player.js
@@ -1,22 +1,7 @@
-// Player & bot AI
-export class Player {
-  constructor(id,name,hp=100){
-    this.id=id;
-    this.name=name;
-    this.hp=hp;
-    this.x=0;
-    this.y=0;
-    this.weapon={name:"Rifle", damage:10};
-  }
-
-  takeDamage(dmg){
-    this.hp -= dmg;
-    if(this.hp<0) this.hp=0;
-  }
-}
-
-// Default bot AI
-export const players = [];
-for(let i=0;i<5;i++){
-  players.push(new Player(`bot${i}`,`Bot${i}`));
-}
+export const player = {
+  x: 400,
+  y: 300,
+  hp: 100,
+  size: 32,
+  speed: 3
+};
