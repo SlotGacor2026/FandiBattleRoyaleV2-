22fdd734e188f7bf56a6d18bd2bbb9bc7dbf4fda
SlotGacor2026 <fandieko758@gmail.com>
2026-01-20 19:55:21 +0700

    Update socket.js

--- js/net/socket.js
+++ js/net/socket.js
@@ -1,10 +1,3 @@
-import { CONFIG } from "../config.js"
-import io from "https://cdn.socket.io/4.6.1/socket.io.esm.min.js"
-
-export const socket = io(CONFIG.SOCKET_URL, {
-  auth: { token: localStorage.getItem("token") }
-})
-
-socket.on("connect", () => console.log("Connected to server"))
-socket.on("disconnect", () => console.log("Disconnected"))
-socket.on("gameState", (state) => console.log("GameState:", state))
+export function initSocket() {
+  console.log("Socket: single player mode");
+}
