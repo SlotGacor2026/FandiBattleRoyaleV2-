53b1fe1820b39d5ee8ff7e55ded07e900ba5535d
SlotGacor2026 <fandieko758@gmail.com>
2026-01-20 21:26:16 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
-alert("MAIN.JS JALAN");
+// CONFIG
+import { CONFIG } from "./config.js"
+
+// GAME
+import { Bot } from "./game/bot.js"
+import { Player } from "./game/player.js"
+
+// UI
+import { initHUD } from "./ui/hud.js"
+
+// START
+console.log("GAME START:", CONFIG.GAME_NAME)
+
+const bots = []
+for (let i = 0; i < CONFIG.MAX_BOT; i++) {
+  bots.push(new Bot(i, Math.random() * 500, Math.random() * 500))
+}
+
+setInterval(() => {
+  bots.forEach(bot => bot.move())
+}, 1000)
+
+initHUD()
