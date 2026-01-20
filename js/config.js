507f037afc6cd2bd507df398dbe935531c5a685c
SlotGacor2026 <fandieko758@gmail.com>
2026-01-20 21:23:46 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,7 @@
 export const CONFIG = {
   GAME_NAME: "FandiBattleRoyaleV2",
-  WIDTH: 800,
-  HEIGHT: 500,
-  BOT_COUNT: 5
+  MODE: "singleplayer", // aman tanpa server
+  MAX_BOT: 10,
+  MAP_SIZE: 3000,
+  ZONE_DAMAGE: 5
 }
