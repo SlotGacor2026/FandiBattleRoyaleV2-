1113bc580c74bf5a1e6e798eb0a0a41870232754
SlotGacor2026 <fandieko758@gmail.com>
2026-01-20 20:31:14 +0700

    Update hud.js

--- js/ui/hud.js
+++ js/ui/hud.js
@@ -1,6 +1,3 @@
-export function updateHUD(mainPlayer, bots){
-  document.getElementById("health").innerText = `HP: ${mainPlayer.hp}`;
-  document.getElementById("coin").innerText = `Coin: 0`;
-  document.getElementById("diamond").innerText = `Diamond: 0`;
-  document.getElementById("inventory").innerText = `Inventory: Empty`;
+export class HUD {
+  update() {}
 }
