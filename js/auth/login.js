7c88ca7bac80598d241c201c27d3505ec297c2c1
SlotGacor2026 <fandieko758@gmail.com>
2026-01-20 20:16:50 +0700

    Update login.js

--- js/auth/login.js
+++ js/auth/login.js
@@ -1,10 +1,5 @@
-export function initLogin(onStart) {
-  const btn = document.getElementById("loginBtn");
-  if (!btn) return;
-
-  btn.onclick = () => {
-    document.getElementById("loginScreen").style.display = "none";
-    document.getElementById("gameScreen").style.display = "block";
-    onStart();
-  };
+export class Login {
+  login(username) {
+    localStorage.setItem("user", username)
+  }
 }
